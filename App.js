c4a67f5d172ce4ac3f2ed35aa77bd8f59dcb575d
pwambui93@gmail.com <pwambui93@gmail.com>
2022-04-26 11:44:55 +0300

    register screen

--- App.js
+++ App.js
@@ -1,32 +1,9 @@
-import ImageInput from "./app/reusableComponents/ImageInput";
 import Screen from "./app/reusableComponents/Screen";
-import * as ImagePicker from "expo-image-picker";
-import { FlatList } from "react-native";
-
-import react, { useState, useEffect } from "react";
+import RegisterScreen from "./app/screens/RegisterScreen";
 export default function App() {
-  const [permission, setPermission] = useState(false);
-  const [imageUri, setImageUri] = useState([]);
-
-  const askForPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted) return alert("You need to permit media permission");
-
-    setPermission(granted);
-  };
-
-  useEffect(() => {
-    askForPermission();
-  }, []);
-
   return (
     <Screen>
-      <FlatList />
-      {/* <ImageInput
-        imageUri={imageUri}
-        permission={permission}
-        setImageUri={setImageUri}
-      /> */}
+      <RegisterScreen />
     </Screen>
   );
 }
